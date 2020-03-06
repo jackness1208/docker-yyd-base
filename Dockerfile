2f58699c8c835a0090c1468678012541371cd609
liudaojie <liudaojie@yy.com>
2020-03-07 01:18:46 +0800

    feat: first commit

---
+++ Dockerfile
@@ -0,0 +1,2 @@
+FROM node:10
+RUN echo 'hello world'
---
+++ yyd.config.js
@@ -0,0 +1,22 @@
+const pkg = require('./package.json');
+module.exports = {
+  // docker 名称
+  repository: 'yyd-base',
+  // tag
+  tag: pkg.version,
+  // 推送的
+  pushHost: '',
+  pushPrefix: '',
+  // 自动 更新 history
+  rewriteHistory: true
+
+  // port 映射
+  // portMap: {
+  //   80:80
+  // },
+
+  // 挂载设置 [本地目录:docker 目录]
+  // volume: {
+  //   './autorun': '/autorun'
+  // },
+};
